6bb9e2ce4387f036c76345e209f6dd95b7026795
shi-n <shi-n_xyz@cloudsquare.jp>
2021-01-09 17:39:06 +0900

    init

--- ExamplePDFKit/ViewController.swift
+++ ExamplePDFKit/ViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import PDFKit
 
 class ViewController: UIViewController {
+    
+    var pdfView: PDFView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.pdfView = PDFView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        self.pdfView.backgroundColor = .blue
+        self.view.addSubview(self.pdfView)
     }
 
 
