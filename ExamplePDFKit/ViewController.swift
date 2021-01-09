0ce63715216f68feec8d7615ab8ca668e34d190b
shi-n <shi-n_xyz@cloudsquare.jp>
2021-01-10 07:53:49 +0900

    Add document.

--- ExamplePDFKit/ViewController.swift
+++ ExamplePDFKit/ViewController.swift
@@ -14,17 +14,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.createPDFView()
+        self.loadDocument(bookurl: Bundle.main.url(forResource: "progit.ja", withExtension: "pdf")!)
+    }
+    
+    func createPDFView() {
         self.pdfView = PDFView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
         self.pdfView.backgroundColor = .blue
         self.view.addSubview(self.pdfView)
+        
         self.pdfView.translatesAutoresizingMaskIntoConstraints = false
         self.pdfView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
         self.pdfView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         self.pdfView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
         self.pdfView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
     }
-
-
+    
+    func loadDocument(bookurl: URL) {
+        self.pdfView.document = PDFDocument(url: bookurl)
+    }
 }
-
