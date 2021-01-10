4059c13da909115e9af364d2744be178feb8b4d4
shi-n <shi-n_xyz@cloudsquare.jp>
2021-01-10 21:26:11 +0900

    Update any.

---
+++ ExamplePDFKit/Extension.swift
@@ -0,0 +1,34 @@
+//
+//  Extension.swift
+//  ExamplePDFKit
+//
+//  Created by Shin Inaba on 2021/01/10.
+//
+
+import Foundation
+import UIKit
+
+extension UIDeviceOrientation {
+    func toString() -> String {
+        var result = ""
+        switch self.rawValue {
+        case 0:
+            result = "unknown"
+        case 1:
+            result = "portrait"
+        case 2:
+            result = "portraitUpsideDown"
+        case 3:
+            result = "landscapeLeft"
+        case 4:
+            result = "landscapeRight"
+        case 5:
+            result = "faceUp"
+        case 6:
+            result = "faceDown "
+        default:
+            break
+        }
+        return result
+    }
+}
--- ExamplePDFKit/ViewController.swift
+++ ExamplePDFKit/ViewController.swift
@@ -10,28 +10,109 @@ import PDFKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var baseView: UIView!
+
     var pdfView: PDFView!
+    var turnPageCount: Int = 1
+    var nowPage: Int = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.createPDFView()
-        self.loadDocument(bookurl: Bundle.main.url(forResource: "progit.ja", withExtension: "pdf")!)
+        let notification = NotificationCenter.default
+        notification.addObserver(self, selector: #selector(self.orientationDidChangeNotification(_:)), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
     
-    func createPDFView() {
-        self.pdfView = PDFView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
-        self.pdfView.backgroundColor = .blue
-        self.view.addSubview(self.pdfView)
+    @objc func orientationDidChangeNotification(_ notification: Notification) {
+        guard self.shouldAutorotate else {
+            return
+        }
+        let device = UIDevice.current
+        print("Device orientation:\(device.orientation.toString())")
+        self.createPDFView(orientation: device.orientation)
+        self.loadDocument(bookurl: Bundle.main.url(forResource: "progit.ja", withExtension: "pdf")!)
+        self.goPage()
+    }
+    
+    func addGesture() {
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.turnPage(sender:)))
+        swipeLeft.direction = .left
+        self.pdfView.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.turnPage(sender:)))
+        swipeRight.direction = .right
+        self.pdfView.addGestureRecognizer(swipeRight)
+    }
+
+    @objc func turnPage(sender: UISwipeGestureRecognizer) {
+        let direction = sender.direction
+        switch direction {
+        case .left:
+            if let page = self.pdfView.currentPage?.pageRef?.pageNumber {
+                self.nowPage = page + self.turnPageCount
+                if self.nowPage > self.pdfView.document!.pageCount {
+                    self.nowPage = self.pdfView.document!.pageCount
+                }
+            }
+        case .right:
+            if let page = self.pdfView.currentPage?.pageRef?.pageNumber {
+                self.nowPage = page - self.turnPageCount
+                if self.nowPage < 1 {
+                    self.nowPage = 1
+                }
+            }
+        default:
+            break
+        }
+        self.goPage()
+    }
+
+    func createPDFView(orientation: UIDeviceOrientation) {
+        guard orientation != .faceUp && orientation != .faceDown && orientation != .unknown else {
+            return
+        }
+        self.clearSunView(view: self.baseView)
+
+        self.pdfView = PDFView(frame: CGRect(x: 0, y: 0, width: self.baseView.frame.width, height: self.baseView.frame.height))
+        self.baseView.addSubview(self.pdfView)
+
+        switch orientation {
+        case .portrait, .portraitUpsideDown:
+            self.pdfView.displayMode = .singlePage
+            self.turnPageCount = 1
+        case .landscapeLeft, .landscapeRight:
+            self.pdfView.displayMode = .twoUp
+            self.turnPageCount = 2
+        default:
+            break
+        }
+        self.pdfView.autoScales = true
         
-        self.pdfView.translatesAutoresizingMaskIntoConstraints = false
-        self.pdfView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        self.pdfView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-        self.pdfView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        self.pdfView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.addConstraintPDFView()
+        self.addGesture()
     }
     
     func loadDocument(bookurl: URL) {
         self.pdfView.document = PDFDocument(url: bookurl)
     }
+    
+    func clearSunView(view: UIView) {
+        for subview in view.subviews {
+            subview.removeFromSuperview()
+        }
+    }
+    
+    func addConstraintPDFView() {
+        self.pdfView.translatesAutoresizingMaskIntoConstraints = false
+        self.pdfView.topAnchor.constraint(equalTo: self.baseView.topAnchor).isActive = true
+        self.pdfView.bottomAnchor.constraint(equalTo: self.baseView.bottomAnchor).isActive = true
+        self.pdfView.leadingAnchor.constraint(equalTo: self.baseView.leadingAnchor).isActive = true
+        self.pdfView.trailingAnchor.constraint(equalTo: self.baseView.trailingAnchor).isActive = true
+    }
+    
+    func goPage() {
+        let pdfPage = self.pdfView.document?.page(at: self.nowPage - 1)
+        self.pdfView.go(to: pdfPage!)
+    }
+
 }
